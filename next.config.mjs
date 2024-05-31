61c8182f6c8e16f39421e306aebbd76e9e1dd8c2
Dominic Leon Hackett <dominichackett@gmail.com>
2024-05-31 11:44:45 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,10 +6,7 @@ const nextConfig = {
       return config;
     }
     ,typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
+      /
       ignoreBuildErrors: true,
     }
   };
